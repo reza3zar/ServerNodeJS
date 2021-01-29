c2e2d0d326f7d4579085d4a4617b7b6ec949f8b7
Reza Younesi <younesi.nima@gmail.com>
2021-01-29 10:30:20 -0500

    v2

--- app.js
+++ app.js
@@ -31,12 +31,14 @@ mongoose.connect(process.env.DB_CONNECTION_ONLINE,{useNewUrlParser: true, useUni
 const http = require('http');
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
 
 server.listen(port,() => {
+    app.use('/posts',postRouter); 
+app.use('/login',loginRouter); 
   console.log(`Server running at port `+port);
 });
